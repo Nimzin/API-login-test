77072c903bf37e4d6c900eebb8843e9872761aff
55229 <55229@DESKTOP-OTFAROA>
2024-11-06 14:37:56 -0300

    Adicionar arquivos de projeto.

---
+++ API login test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using API_login_test;
+
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://dummyjson.com/users/5/");
+        Usuarios usuario = JsonSerializer.Deserialize<Usuarios>(resposta);
+        usuario.Informacoes();
+        
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
---
+++ API login test/Usuarios.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace API_login_test
+{
+    internal class Usuarios
+    {
+        [JsonPropertyName("firstName")]
+        public string Nome { get; set; }
+        [JsonPropertyName("gender")]
+        public string Sexo { get; set; }
+        [JsonPropertyName("city")]
+        public string Cidade { get; set; }
+
+   public void Informacoes()
+        {
+            Console.WriteLine($" Nome: {Nome}");
+            Console.WriteLine($" Sexo: {Sexo}");
+            Console.WriteLine($" Cidade: {Cidade}");
+        }
+    }
+
+
+}
